1e4efa2cba1644510cd8ebfb8bc0820dab249776
i-xi-dev <naoyuki.isono@gmail.com>
2021-12-01 21:42:00 +0900

    Update dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+import path from "node:path";
+
+export default {
+  mode: "development",
+  entry: {
+    "/": "./src/index.ts",
+  },
+  output: {
+    filename: "[name]index.js",
+    path: path.join(process.cwd(), "dist"),
+    library: {
+      type: "module",
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      }
+    ],
+  },
+  resolve: {
+    extensions: [
+      ".ts",
+      ".js",
+    ],
+  },
+  experiments: {
+    outputModule: true,
+  },
+};
