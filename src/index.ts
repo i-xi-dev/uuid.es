0bbadb35e3fc51c316695d02515889c74e8c22e0
i-xi-dev <naoyuki.isono@gmail.com>
2022-01-29 16:39:10 +0900

    2.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 //
 
 export {
+  type UuidFormatOptions,
   Uuid,
 } from "./uuid";
--- src/uuid.ts
+++ src/uuid.ts
@@ -3,13 +3,25 @@
 import { type uint8 } from "@i-xi-dev/fundamental";
 import { ByteSequence } from "@i-xi-dev/bytes";
 
+/**
+ * The object with the following optional fields.
+ */
 type UuidFormatOptions = {
+  /**
+   * Whether the formatted string is uppercase or not.
+   * The default is `false`.
+   */
   upperCase?: boolean;
+
+  /**
+   * Whether to use hyphens to join fields.
+   */
   hyphenate?: boolean;
-}
+};
 
 /**
- * UUID
+ * The object representation of UUID.
+ * The `Uuid` instances are immutable.
  * 
  * Implements version 4 UUID.
  * 
@@ -41,10 +53,98 @@ class Uuid {
     Object.freeze(this);
   }
 
+  /**
+   * Gets the [variant](https://datatracker.ietf.org/doc/html/rfc4122#section-4.1.1) of this UUID.
+   */
+  get type(): number {
+    const byte8 = this.#clockSeqAndReserved.getUint8View()[0] as uint8;
+    if ((byte8 & 0b11100000) === 0b11100000) {
+      return 7;
+    }
+    else if ((byte8 & 0b11000000) === 0b11000000) {
+      return 6;
+    }
+    else if ((byte8 & 0b10000000) === 0b10000000) {
+      return 2;
+    }
+    else { // if ((byte8 & 0b00000000) === 0b00000000) {
+      return 0;
+    }
+  }
+
+  /**
+   * The alias for the `type` getter.
+   */
+  get variant(): number {
+    return this.type;
+  }
+
+  /**
+   * Gets the [version](https://datatracker.ietf.org/doc/html/rfc4122#section-4.1.3) of this UUID.
+   */
+  get subtype(): number {
+    if (this.type === 2) {
+      const byte6 = this.#timeHighAndVersion.getUint8View()[0] as uint8;
+      if ((byte6 & 0b01010000) === 0b01010000) {
+        return 5;
+      }
+      else if ((byte6 & 0b01000000) === 0b01000000) {
+        return 4;
+      }
+      else if ((byte6 & 0b00110000) === 0b00110000) {
+        return 3;
+      }
+      else if ((byte6 & 0b00100000) === 0b00100000) {
+        return 2;
+      }
+      else if ((byte6 & 0b00010000) === 0b00010000) {
+        return 1;
+      }
+    }
+    return Number.NaN;
+  }
+
+  /**
+   * The alias for the `subtype` getter.
+   */
+  get version(): number {
+    return this.version;
+  }
+
+  /**
+   * Creates an `Uuid` object that represents the [nil UUID](https://datatracker.ietf.org/doc/html/rfc4122#section-4.1.7).
+   * 
+   * @returns An `Uuid` object that represents the nil UUID.
+   */
   static nil(): Uuid {
     return new Uuid(ByteSequence.allocate(16));
   }
 
+  /**
+   * Creates an `Uuid` object that represents the version 4 UUID.
+   * 
+   * @returns An `Uuid` object that represents the version 4 UUID.
+   */
+  static generateRandom(): Uuid {
+    const randomBytes = ByteSequence.generateRandom(16);
+    const uint8View = randomBytes.getUint8View();
+
+    // timeHighAndVersionの先頭4ビット（7バイト目の上位4ビット）は0100₂固定（13桁目の文字列表現は"4"固定）
+    uint8View[6] = (uint8View[6] as uint8) & 0x0F | 0x40;
+
+    // clockSeqAndReservedの先頭2ビット（9バイト目の上位2ビット）は10₂固定（17桁目の文字列表現は"8","9","A","B"のどれか）
+    uint8View[8] = (uint8View[8] as uint8) & 0x3F | 0x80;
+
+    return new Uuid(randomBytes);
+  }
+
+  /**
+   * Creates an `Uuid` object from a string that represents the UUID.
+   * 
+   * @returns An `Uuid` object.
+ * @throws {TypeError} The `uuidString` is not type of string.
+ * @throws {RangeError} The `uuidString` is not reperesent the UUID.
+   */
   static fromString(uuidString: string): Uuid {
     if (typeof uuidString !== "string") {
       throw new TypeError("uuidString");
@@ -58,6 +158,12 @@ class Uuid {
     return new Uuid(parsedBytes);
   }
 
+  /**
+   * Returns a string representation of this UUID.
+   * 
+   * @param options - The `UuidFormatOptions` dictionary.
+   * @returns A string representation of this UUID.
+   */
   format(options: UuidFormatOptions = {}): string {
     const upperCase = (typeof options?.upperCase === "boolean") ? options.upperCase : false;
     const hyphenate = (typeof options?.hyphenate === "boolean") ? options.hyphenate : true;
@@ -90,6 +196,13 @@ class Uuid {
     return new URL("urn:uuid:" + this.toString());
   }
 
+  /**
+   * Determines whether this UUID is equal to the UUID represented by another object.
+   * 
+   * @param other - The object or string that represents a UUID.
+   * @returns If this is equal to the specified UUID, `true`; otherwise, `false`.
+   * @throws {TypeError} The `other` is not type of `Uuid` or string.
+   */
   equals(other: Uuid | string): boolean {
     if (other instanceof Uuid) {
       return this.#bytes.equals(other.#bytes);
@@ -103,59 +216,7 @@ class Uuid {
         return false;
       }
     }
-    return false;
-  }
-
-  get type(): number {
-    const byte8 = this.#clockSeqAndReserved.getUint8View()[0] as uint8;
-    if ((byte8 & 0b11100000) === 0b11100000) {
-      return 7;
-    }
-    else if ((byte8 & 0b11000000) === 0b11000000) {
-      return 6;
-    }
-    else if ((byte8 & 0b10000000) === 0b10000000) {
-      return 2;
-    }
-    else { // if ((byte8 & 0b00000000) === 0b00000000) {
-      return 0;
-    }
-  }
-
-  get variant(): number {
-    return this.type;
-  }
-
-  get version(): number {
-    if (this.type === 2) {
-
-    }
-    else {
-      return Number.NaN;
-    }
-  }
-
-  get subtype(): number {
-    return this.version;
-  }
-
-
-  /**
-   * Creates an `Uuid` object that represents the version 4 UUID.
-   * 
-   * @returns An `Uuid` object that represents the version 4 UUID.
-   */
-  static generateRandom(): Uuid {
-    const randomBytes = ByteSequence.generateRandom(16);
-    const uint8View = randomBytes.getUint8View();
-
-    // timeHighAndVersionの先頭4ビット（7バイト目の上位4ビット）は0100₂固定（13桁目の文字列表現は"4"固定）
-    uint8View[6] = (uint8View[6] as uint8) & 0x0F | 0x40;
-
-    // clockSeqAndReservedの先頭2ビット（9バイト目の上位2ビット）は10₂固定（17桁目の文字列表現は"8","9","A","B"のどれか）
-    uint8View[8] = (uint8View[8] as uint8) & 0x3F | 0x80;
-
-    return new Uuid(randomBytes);
+    throw new TypeError("other");
   }
 }
 Object.freeze(Uuid);
