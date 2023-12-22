fc25d6004e046ace50965f6ab75e036ca94a9e23
i-xi-dev <naoyuki.isono@gmail.com>
2023-12-22 21:55:09 +0900

    3.0.15

--- build_npm.ts
+++ build_npm.ts
@@ -15,7 +15,7 @@ await build({
   rootTestDir: "./tests",
   package: {
     name: "@i-xi-dev/uuid",
-    version: "3.0.14",
+    version: "3.0.15",
     description:
       "A JavaScript UUID generator, implements the version 4 UUID defined in RFC 4122.",
     license: "MIT",
