0289deb6313bb296e1c437d842df60513dacc12b
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-28 16:39:31 +0900

    3.0.19

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.15/mod.ts";
-export { BufferUtils } from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.0/mod.ts";
-export { BytesFormat } from "https://raw.githubusercontent.com/i-xi-dev/bytes-format.es/1.0.27/mod.ts";
-export { Digest } from "https://raw.githubusercontent.com/i-xi-dev/digest.es/3.0.7/mod.ts";
-export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.0.0/mod.ts";
+export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.16/mod.ts";
+export { BufferUtils } from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.1/mod.ts";
+export { BytesFormat } from "https://raw.githubusercontent.com/i-xi-dev/bytes-format.es/1.0.28/mod.ts";
+export { Digest } from "https://raw.githubusercontent.com/i-xi-dev/digest.es/3.0.8/mod.ts";
+export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.1.0/mod.ts";
