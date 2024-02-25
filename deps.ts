60a123b939f874ec25b3ebc70115e146f6a9f3cf
i-xi-dev <naoyuki.isono@gmail.com>
2024-02-25 22:49:47 +0900

    3.2.1

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.16/mod.ts";
-export { BufferUtils } from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.1/mod.ts";
-export { BytesFormat } from "https://raw.githubusercontent.com/i-xi-dev/bytes-format.es/1.0.28/mod.ts";
-export { Digest } from "https://raw.githubusercontent.com/i-xi-dev/digest.es/3.0.8/mod.ts";
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.3/mod.ts";
+export { _crypto } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.17/mod.ts";
+export { BufferUtils } from "https://raw.githubusercontent.com/i-xi-dev/buffer-utils.es/6.2.2/mod.ts";
+export { BytesFormat } from "https://raw.githubusercontent.com/i-xi-dev/bytes-format.es/1.0.29/mod.ts";
+export { Digest } from "https://raw.githubusercontent.com/i-xi-dev/digest.es/3.0.9/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.4/mod.ts";
 export {
   SafeInteger,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.1.0/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.3.0/mod.ts";
